c66af52580da561df0de99d43b6ddd366066fe57
Brian Sohn <kolpp15@gmail.com>
2021-09-13 16:16:50 -0700

    create Player class and tested with main.rb

---
+++ main.rb
@@ -0,0 +1,10 @@
+require './game'
+require './player'
+require './question'
+
+# player1 = Player.new('brian')
+# puts player1.name
+# puts player1.lives
+
+question1 = Question.new(1)
+puts question1.start
